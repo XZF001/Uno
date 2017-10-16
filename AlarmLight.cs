99ce80087bd0c49f2ae2421be76fa3128f10dfff
XZF001 <1132685782@qq.com>
2017-10-16 17:24:20 +0800

    ss
    xx

---
+++ AlarmLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour {
+	public float fadeSpeed=2f;
+	public float hightIntensity=4f;
+	public float lowIntensity=0.5f;
+	public float changeMargin=0.2f;
+	public bool alarnOn;
+	private float targetIntensity;
+	private Light alarmLight;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
